859b20ba40c02a6fb8b0778574cbff388e70128d
st47 <siva_teja47@yahoo.com>
2024-10-03 00:19:01 -0700

    Flip on click

--- spasjsx/mgfiles/Memorygame.jsx
+++ spasjsx/mgfiles/Memorygame.jsx
@@ -3,23 +3,13 @@ import '../../spscss/memorygame.css'
 
 import cardimage from '../../spastatic/card.png'
 
-const content = [1, 2, 3, 4]
-const twod = content.map((each) => { return [each, each] })
-function flatten(twodarray) {
-    return twodarray.reduce((accu, curval) => {
-        if (Array.isArray(curval)) {
-            return accu.concat(flatten(curval))
-        }
-        else {
-            accu.push(curval)
-            return accu
-        }
-    }, [])
-}
-const contentready = flatten(twod).sort(() => { return Math.random() - 0.5 })
+const content = [1, 2, 3, 4, 1, 2, 3, 4]
+
+
+const contentready = content.sort(() => { return Math.random() - 0.5 }).map((each) => { return { idee: Math.random(), value: each, status: false, scored: false } })
 
 const Memorygame = () => {
-
+    const [cardsForScreen, setCardsForScreen] = useState(contentready)
 
     const [firstChoice, setFirstChoice] = useState({ cchoice: 'x', sstatus: false })
     const [secondChoice, setSecondChoice] = useState({ cchoice: 'y', sstatus: false })
@@ -28,13 +18,20 @@ const Memorygame = () => {
     const [fun, setFun] = useState(27)
 
     function anycrdclk(arg) {
+
+        setCardsForScreen((prev) => {
+            return prev.map((each) => {
+                return each.idee == arg.idee ? { ...each, status: true } : each
+            })
+        })
+
         if (firstChoice.sstatus !== true) {
-            setFirstChoice({ cchoice: arg, sstatus: true })
+            setFirstChoice({ cchoice: arg.value, sstatus: true })
 
         }
 
         else if (secondChoice.sstatus !== true) {
-            setSecondChoice({ cchoice: arg, sstatus: true })
+            setSecondChoice({ cchoice: arg.value, sstatus: true })
 
             setFun(Math.random())
 
@@ -44,7 +41,19 @@ const Memorygame = () => {
     useEffect(() => {
         if (firstChoice.cchoice == secondChoice.cchoice) {
             setScore((prev) => { return prev + 1 })
+            setCardsForScreen((prev) => {
+                return prev.map((each) => {
+                    return each.value == firstChoice.cchoice ? { ...each, scored: true } : each
+                })
+            })
+        } else {
+            setCardsForScreen((prev) => {
+                return prev.map((each) => {
+                    return !each.scored ? { ...each, status: false } : each
+                })
+            })
         }
+
 
         setFirstChoice(
             { cchoice: 'x', sstatus: false }
@@ -56,27 +65,22 @@ const Memorygame = () => {
     }, [fun])
 
 
+    console.log(cardsForScreen)
+
 
     return (
         <div className="heromem">
             <h1 className='memscore'>score: {score}</h1>
             <div className="memgrid">
 
-                {contentready.map((each) => {
+                {cardsForScreen.map((each) => {
                     return <div className="memcard" onClick={() => { anycrdclk(each) }}>
-                        <div className="headsface">
+                        <div className="headsface" style={{ transform: each.status && 'rotateY(180deg)' }}>
                             <img src={cardimage} />
                         </div>
-                        <div className="tailsface">{each}</div>
+                        <div className="tailsface" style={{ transform: each.status && 'rotateY(0deg)' }}>{each.value}</div>
                     </div>
                 })}
-
-                <div className="memcard">
-                    <div className="headsface">
-                        <img src={cardimage} />
-                    </div>
-                    <div className="tailsface">joker</div>
-                </div>
             </div>
         </div>
     )
